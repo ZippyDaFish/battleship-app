6a5274409975c7fc84dc6495c645772519cac510
ZippyDaFish <andrew.riker21@gmail.com>
2023-02-07 12:19:55 -0500

    Add fire on computer board prerequisites

--- src/index.js
+++ src/index.js
@@ -24,18 +24,28 @@ function gameboardFactory(){
             for (c = 0; c < (rows * cols); c++) {
                 let cell = document.createElement("div");
                 cell.setAttribute('id', c.toString());
-                cell.addEventListener('click', function handleClick(){console.log(cell.id)});
+                cell.addEventListener('click', function handleClick(){fireOnComputerBoard(cell);});
                 this.board[c] = cell;
                 container.appendChild(cell).className = "cell";
             };
         },
-        fire(){},
-        hitCheck(){}
+        fire(cell){
+            if(cell.innerText != ""){
+                return;
+            }
+            this.hitCheck(cell.id);
+        },
+        hitCheck(cellID){
+            console.log(cellID);
+        }
     }
+}
+
+function fireOnComputerBoard(cell){
+    computerBoard.fire(cell);
 }
 
 let playerBoard = gameboardFactory();
 playerBoard.makeBoard(10,10, document.getElementById('player-board-container'));
 let computerBoard = gameboardFactory();
 computerBoard.makeBoard(10,10, document.getElementById('computer-board-container'))
-console.log(computerBoard.board);
